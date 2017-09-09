fed01f6d36239441c5e759a3d0daa643ceda0f72
JennyMcC <jennymccargish@gmail.com>
2017-09-09 13:19:45 -0400

    setup

---
+++ src/main/java/dormGardens/com/dormGardens/DormGardensApplication.java
@@ -0,0 +1,12 @@
+package dormGardens.com.dormGardens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormGardensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormGardensApplication.class, args);
+	}
+}
---
+++ src/test/java/dormGardens/com/dormGardens/DormGardensApplicationTests.java
@@ -0,0 +1,16 @@
+package dormGardens.com.dormGardens;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DormGardensApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
