f4f33e96c76adb99ecfca6ca2f8148aa457b86b9
JennyMcC <jennymccargish@gmail.com>
2017-09-11 18:41:30 -0400

    damn thymeleaf

--- src/main/java/dormGardens/com/dormGardens/DormGardensPopulator.java
+++ src/main/java/dormGardens/com/dormGardens/DormGardensPopulator.java
@@ -32,10 +32,24 @@ public class DormGardensPopulator implements CommandLineRunner {
 		Plant plant1 = new Plant("Ironwood Succulent Planter", "/images/plant1.jpg", "15", price15, medium);
 		Plant plant2 = new Plant("Marblewood Succulent Planter", "/images/plant2.jpg", "15", price15, light);
 		Plant plant3 = new Plant("African Padauk Succulent Planter", "/images/plant3.jpg", "15", price15, medium);
+		Plant plant4 = new Plant("Walnut Succulent Planter", "images/plant4.jpg", "20", price20, medium);
+		Plant plant5 = new Plant("Oak Succulent Planter", "images/plant5.jpg", "20", price20, light);
+		Plant plant6 = new Plant("Cedar Succulent Planter", "images/plant6.jpg", "20", price20, medium);
+		Plant plant7 = new Plant("Ironwood Succulent Planter", "images/plant7.jpg", "15", price15, medium);
+		Plant plant8 = new Plant("Cedar Succulent Planter", "images/plant8.jpg", "20", price20, medium);
+		Plant plant9 = new Plant("Purpleheart Succulent Planter", "images/plant9.jpg", "20", price20, dark);
+		
+		
 		
 		plantRepo.save(plant1);
 		plantRepo.save(plant2);
 		plantRepo.save(plant3);
+		plantRepo.save(plant4);
+		plantRepo.save(plant5);
+		plantRepo.save(plant6);
+		plantRepo.save(plant7);
+		plantRepo.save(plant8);
+		plantRepo.save(plant9);
 		
 	}
 }
