c8c0116279eb934310d7bcd5f1ff1ccdf58dcdd2
JennyMcC <jennymccargish@gmail.com>
2017-09-11 22:14:10 -0400

    links and some css

--- src/main/java/dormGardens/com/dormGardens/DormGardensPopulator.java
+++ src/main/java/dormGardens/com/dormGardens/DormGardensPopulator.java
@@ -18,23 +18,25 @@ public class DormGardensPopulator implements CommandLineRunner {
 		
 		Tags price15 = new Tags("$15 plants");
 		Tags price20 = new Tags("$20 plants");
-		Tags light = new Tags("light wood");
-		Tags medium = new Tags("medium wood");
-		Tags dark = new Tags("dark wood");
+		Tags light = new Tags("light colored planter");
+		Tags medium = new Tags("medium colored planter");
+		Tags dark = new Tags("dark colored planter");
+		Tags dipped = new Tags("paint dipped");
 		
 		tagsRepo.save(price15);
 		tagsRepo.save(price20);
 		tagsRepo.save(light);
 		tagsRepo.save(medium);
 		tagsRepo.save(dark);
+		tagsRepo.save(dipped);
 		
 		
 		Plant plant1 = new Plant("Ironwood Succulent Planter", "/images/plant1.jpg", "15", price15, medium);
 		Plant plant2 = new Plant("Marblewood Succulent Planter", "/images/plant2.jpg", "15", price15, light);
-		Plant plant3 = new Plant("African Padauk Succulent Planter", "/images/plant3.jpg", "15", price15, medium);
+		Plant plant3 = new Plant("African Padauk Succulent Planter", "/images/plant3.jpg", "15", price15, dark);
 		Plant plant4 = new Plant("Walnut Succulent Planter", "images/plant4.jpg", "20", price20, medium);
 		Plant plant5 = new Plant("Oak Succulent Planter", "images/plant5.jpg", "20", price20, light);
-		Plant plant6 = new Plant("Cedar Succulent Planter", "images/plant6.jpg", "20", price20, medium);
+		Plant plant6 = new Plant("Cedar Succulent Planter", "images/plant6.jpg", "20", price20, light);
 		Plant plant7 = new Plant("Ironwood Succulent Planter", "images/plant7.jpg", "15", price15, medium);
 		Plant plant8 = new Plant("Cedar Succulent Planter", "images/plant8.jpg", "20", price20, medium);
 		Plant plant9 = new Plant("Purpleheart Succulent Planter", "images/plant9.jpg", "20", price20, dark);
