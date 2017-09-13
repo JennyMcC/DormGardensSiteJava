ffe7494fc81fc00fb2a80e8f7baf6d6401a88b1a
JennyMcC <jennymccargish@gmail.com>
2017-09-13 17:06:28 -0400

    attempting to change to single page

---
+++ bin/static/script.js
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+var initPage = function(jsonResponseUrl) {
+	
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var allPlants = JSON.parse(xhttp.responseText);
+			var galleryContent = [];
+			var price;
+			var picture;
+			
+			for (var i = 0; i < allPlants.length; i++) {
+				plant = allPlants[i];
+				
+				galleryContent[i] = setGallery(plant);
+				
+				
+			}
+			
+			
+			function setGallery(plant) {
+				var tagNames = "";
+				for (var j = 0; j < plant.tags.length; j++) {
+					tagNames += '<li >' + plant.tags[j].displayName + '</li>';
+				}
+				
+				var stringContent = '.gallery' + plant.picture + '.price' + plant.price
+				
+				return stringContent;
+			}
+		}
+	}
+	
+	console.log(jsonResponseUrl);
+	xhttp.open("GET", jsonResponseUrl, true);
+	xhttp.send();
+}
+
+
+
+$(document).ready(function() {
+	
+	$('button[name="generatePlantsByFilter"]')
+	.on('click', function() {
+		var selectedFilters = [];
+		$(".filter:checked").each(
+				function() {
+					selectedFilters.push(this.name);
+				});
+		
+		initPage("http://localhost:8080/DormGardens/byFilter/" + selectedFilters);
+	}
+}
